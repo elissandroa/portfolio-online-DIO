e738b85f419cca3a8b98635ac387136ecabdfdd1
elissandroa <elissandroa@hotmail.com>
2024-10-06 19:35:41 -0300

    Preenchendo os dados de Softskills

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,8 +23,15 @@ function updateProfileInfo(profileData){
     email.innerText = profileData.email;
 }
 
+function updateSoftSkills(profileData){
+    const softSkills = document.getElementById('profile.skills.softSkills');
+    softSkills.innerHTML = profileData.skills.softSkills.map( skill => `<li>${skill}</li>`).join('');
+        
+
+}
 
 (async  () => {
     const profileData = await fetchProfileData();
     updateProfileInfo(profileData);
+    updateSoftSkills(profileData);
 })()
